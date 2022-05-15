4cbaf0b38491c37da879673a9a7de5e0133573db
michaellin2 <lin1042612714@gmail.com>
2022-05-15 03:55:33 +0100

    added comments for every page

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
 // App.js
-import React from 'react';
-import { StyleSheet, Image } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from "react";
+import { StyleSheet, Image } from "react-native";
+//import and install the libraries for different navigation
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import HomePage from './Screens/Home';
-import ColorAdjustmentPage from './Screens/ColorAdjustment';
-import SettingPage from './Screens/Setting';
-import WiFiPage from './Screens/WIFI';
-import AddDevicePage from './Screens/AddDevice';
-import homeIcon from './assets/home.png';
-import settingIcon from './assets/setting.png';
+//import every pages
+import HomePage from "./Screens/Home";
+import ColorAdjustmentPage from "./Screens/ColorAdjustment";
+import SettingPage from "./Screens/Setting";
+import WiFiPage from "./Screens/WIFI";
+import AddDevicePage from "./Screens/AddDevice";
+//import different images
+import homeIcon from "./assets/home.png";
+import settingIcon from "./assets/setting.png";
 
+//declare variable for stack navigation and bottom navigation
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
+//stylesheet
 const styles = StyleSheet.create({
   imageIcon: {
     height: 27,
@@ -25,47 +30,57 @@ const styles = StyleSheet.create({
 
 function App() {
   return (
+    //create tab navigator with customised icon
     <Tab.Navigator screenOptions={{ headerShown: false }}>
       <Tab.Screen
         name="home"
-        component={HomePage} 
+        component={HomePage}
         options={{
           tabBarIcon: () => (
-            <Image
-              style={styles.imageIcon}
-              source={homeIcon}
-            />
-          )
-          ,
+            <Image style={styles.imageIcon} source={homeIcon} />
+          ),
         }}
       />
       <Tab.Screen
         name="Setting"
-        component={SettingPage} 
+        component={SettingPage}
         options={{
           tabBarIcon: () => (
-            <Image
-              style={styles.imageIcon}
-              source={settingIcon}
-            />
-          )
-          ,
+            <Image style={styles.imageIcon} source={settingIcon} />
+          ),
         }}
-      />      
+      />
     </Tab.Navigator>
   );
 }
 export default function TabNav() {
   return (
+    //create stack navigation container and declare all the pages 
     <NavigationContainer>
       <Stack.Navigator
         initialRouteName="Home"
         screenOptions={{ headerShown: false }}
       >
-        <Stack.Screen name="Home" component={App} option={{headerShown:false}} />
-        <Stack.Screen name="Color" component={ColorAdjustmentPage} option={{headerShown:false}} />
-        <Stack.Screen name="WIFI" component={WiFiPage} option={{headerShown:false}} />
-        <Stack.Screen name="AddDevice" component={AddDevicePage} option={{headerShown:false}} />
+        <Stack.Screen
+          name="Home"
+          component={App}
+          option={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Color"
+          component={ColorAdjustmentPage}
+          option={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="WIFI"
+          component={WiFiPage}
+          option={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="AddDevice"
+          component={AddDevicePage}
+          option={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
